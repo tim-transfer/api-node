b5cac1cb2e6267da0ebe36d899fdd7f14eb88619
Joachim Guitton <joachim.guitton@gmail.com>
2024-06-18 10:16:56 +0200

    feature : première mise en place des documents.

--- routes/documentRouter.js
+++ routes/documentRouter.js
@@ -1,20 +1,37 @@
-
 import { Router } from "express";
 import controller from "../controllers/documentController.js";
 import signatureLinkController from "../controllers/signatureLinkController.js";
 import passport from "passport";
-import checkSignatureLink from '../middlewares/checkSignatureLink.js'
+import checkSignatureLink from "../middlewares/checkSignatureLink.js";
 const router = Router();
 
-router.put('/add', controller.createDocument);
-router.patch('/update/:id',   passport.authenticate("jwt", { session: false }),
-controller.updateDocument);
-router.delete('/delete/:id', passport.authenticate("jwt", { session: false }), controller.deleteDocument);
+router.put("/document/add", controller.createDocument);
+router.patch(
+  "/document/update/:id",
+  passport.authenticate("jwt", { session: false }),
+  controller.updateDocument
+);
+router.delete(
+  "/document/delete/:id",
+  passport.authenticate("jwt", { session: false }),
+  controller.deleteDocument
+);
 
-router.post('/generate-link', passport.authenticate("jwt", { session: false }), signatureLinkController.generateSignatureLink);
+router.post(
+  "/document/generate-link",
+  passport.authenticate("jwt", { session: false }),
+  signatureLinkController.generateSignatureLink
+);
 
-router.get('/access/:token', passport.authenticate("jwt", { session: false }), checkSignatureLink, (req, res) => {
+router.get(
+  "/document/access/:token",
+  passport.authenticate("jwt", { session: false }),
+  checkSignatureLink,
+  (req, res) => {
     req.link.update({ used: true });
     res.status(200).send(req.document);
-});
+  }
+);
+
+router.get("/documents", controller.getAllDocuments);
 export default router;
